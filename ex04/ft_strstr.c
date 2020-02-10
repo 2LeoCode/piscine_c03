7bc698ba8aafb01749ba18001866c56f3b1b8e90
Your Name <you@example.co>
2020-02-10 21:59:01 +0100

    le bocal on t encule

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,34 +6,39 @@
 /*   By: lsuardi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 09:31:41 by lsuardi           #+#    #+#             */
-/*   Updated: 2020/02/10 14:06:27 by lsuardi          ###   ########.fr       */
+/*   Updated: 2020/02/10 21:41:54 by lsuardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strstr(char *str, char *to_find)
 {
 	unsigned int	i;
+	unsigned int	stock_i;
+	unsigned int	j;
 	char			*point;
 
 	point = 0;
 	i = 0;
-	if (*to_find)
+	while (str[i])
 	{
-		while (str[i])
+		if (!point)
 		{
-			if (!point)
+			if (str[i] == to_find[0])
 			{
-				if (str[i] == *to_find)
+				stock_i = i;
+				j = 0;
+				while (to_find[j])
 				{
-					point = &str[i];
+					if (to_find[j] != str[stock_i])
+						break;
+					stock_i++;
+					j++;
 				}
+				if (!to_find[j])
+					point = &str[i];
 			}
-			i++;
 		}
-	}
-	else
-	{
-		point = str;
+		i++;
 	}
 	return (point);
 }
