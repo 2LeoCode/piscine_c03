23ccb72e8d570deb7a6acffbab2a31097ac18fb2
Your Name <you@example.co>
2020-02-24 12:44:56 +0100

    lol

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,42 +6,47 @@
 /*   By: lsuardi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 21:32:15 by lsuardi           #+#    #+#             */
-/*   Updated: 2020/02/19 13:43:38 by lsuardi          ###   ########.fr       */
+/*   Updated: 2020/02/24 12:43:58 by lsuardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int		ft_strlen(char *str);
-
-char	*ft_strstr(char *str, char *to_find)
-{
-	int		i;
-	int		j;
-	char	*point;
-
-	i = 0;
-	point = 0;
-	while (str[i])
-	{
-		j = 0;
-		while (str[i + j] == to_find[j])
-			j++;
-		if (j >= ft_strlen(to_find))
-		{
-			if (str[ft_strlen(str)] == to_find[ft_strlen(to_find)])
-				point = &str[i];
-			return (point);
-		}
-		i++;
-	}
-	return (point);
-}
 
 int		ft_strlen(char *str)
 {
 	int i;
 
-	i = -1;
-	while (str[++i])
-		continue;
-	return (i - 1);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int		ft_strncmp(char *s1, char *s2, int n)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] && s2[i] && i < n - 1)
+	{
+		if (s1[i] != s2[i])
+			break ;
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == to_find[0])
+		{
+			if (!ft_strncmp(&str[i], to_find, ft_strlen(to_find)))
+				return (&str[i]);
+		}
+		i++;
+	}
+	return (0);
 }
